71326b7cdd4ad46f48f6a7eecb653efcf04379a8
chenqy9 <qingyao.chen@meicloud.com>
2018-12-29 01:43:05 +0800

    更改主题

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "基于Jekyll和GitHub Pages搭建个人博客",
+        "excerpt":"JeKyll JeKyll是一个基于Ruby的博客类静态网站生成器。 Jekyll是一个简单，可扩展的静态站点生成器。您可以使用自己喜欢的标记语言编写文本，然后通过布局来创建静态网站。在整个过程中，您可以调整网站URL的显示方式，布局中显示的数据等 基于JeKyll创建静态网站 安装Ruby开发环境 (以macOS 为例) macOS Mojave 10.14 可省略以下安装步骤 1.1 安装Homebrew和Ruby # Install Homebrew /usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\" brew install ruby 1.2 导出Ruby环境变量 echo 'export PATH=/usr/local/opt/ruby/bin:$PATH' &gt;&gt; ~/.zshrc 1.3 检查Ruby是否安装成功 which ruby /usr/local/opt/ruby/bin/ruby ruby -v ruby 2.6.0p0 (2018-12-25 revision 66547) [x86_64-darwin18] 安装打包器和jekyll gem install --user-install bundler jekyll...","categories": ["文章教程"],
+        "tags": ["博客","Jekyll","GitHub Pages","markdown"],
+        "url": "http://localhost:4000/%E6%96%87%E7%AB%A0%E6%95%99%E7%A8%8B/build-blog-with-jekyll-and-github-pages/",
+        "teaser":null}]
