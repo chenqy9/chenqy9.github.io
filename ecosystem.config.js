ae260874a655070c62410457459d984da561584d
qingyaochen <qingyaochen@tencent.com>
2022-08-24 18:24:56 +0800

    feat: update blog

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'blog',
     script: 'hexo-server.js',
-    watch: ["*.yml"],
-    ignore_watch: ["[\/\\]\./", "node_modules"],
+    watch: ["*.yml", "source"],
+    // ignore_watch: ["[\/\\]\./", "node_modules"],
   },],
 };
---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: 分类
+date: 2019-05-22 15:27:25
+type: "categories"
+---
